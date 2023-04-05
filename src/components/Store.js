52bcf55d56a7867d8ae4e0a75508e51116e4c033
4MN35T <113859255+4MN35T@users.noreply.github.com>
2023-04-06 03:06:06 +0530

    Update Store.js

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
-function Store({ setIsLoggedIn }) {
-    const history = useHistory();
-    const logOutClicked = () => {
-        setIsLoggedIn(false);
-        history.push('/');
+function Store(props) {
+
+    const handleLogout = () => {
+        props.setIsLoggedIn(false);
     }
+    
     return (
         <div className='store'>
             <h2>Welcome to the store!</h2>
-            <button className="logout-btn" onClick={logOutClicked}>Logout</button>
+            <button onClick={handleLogout} className="logout-btn">Logout</button>
         </div>
     );
 }
