f345eb6414ab001543d7d6dbd25f2fd1b2f7881b
4MN35T <113859255+4MN35T@users.noreply.github.com>
2023-04-06 03:05:11 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,42 +1,60 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
-const Login = ({ setIsLoggedIn }) => {
+const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(false);
-    // const [isLogIn, setIsLogIn] = useState(false);
+    const [isLogIn, setIsLogIn] = useState(false);
 
-    const formSubmit = (e) => {
-        e.preventDefault();
-        if (!email || !password || !JSON.parse(localStorage.getItem('user')) || JSON.parse(localStorage.getItem('user')).email != email || JSON.parse(localStorage.getItem('user')).password != password) {
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (email.length < 0 || password.length < 0) {
             setError(true);
-            return;
         }
-        setError(false);
-        setIsLoggedIn(true);
-        setEmail('');
-        setPassword('');
-    }
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user && user.email === email && user.password === password) {
+            props.setIsLoggedIn(true);
+            setIsLogIn(true);
+            setError(false);
+            setEmail("");
+            setPassword("");
+        } else {
+            setError(true);
+        }
+
+        if (isLogIn) {
+            return <Redirect to="/store" />
+        }
+    };
 
     return (
         <div className='login'>
             <h2>Login</h2>
-            <form onSubmit={formSubmit}>
-                {error && <p className='error-para'>"Email or password is invalid"</p>}
+            <form onSubmit={handleSubmit} >
+                {error ? <p className='error-para'>"Email or password is invalid"</p> : null}
                 <div className='email-div'>
                     <label htmlFor="email">Email: </label>
-                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" id="email" />
+                    <input type="email" id="email" value={email} onChange={handleEmailChange} autoComplete="off"/>
                 </div>
                 <div className='password-div'>
                     <label htmlFor="password">Password: </label>
-                    <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" id="password" />
+                    <input type="password" id="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <button type="submit" className='login-btn'>Log In</button>
             </form>
             <div>
-                Don't have an account?
-                <Link to={'/'}><button className='register-link'>Register</button></Link>
+                Don't have an account? 
+                <Link to="/"><button className='register-link'>Register</button></Link>
             </div>
         </div>
     );
