e8926aeb1fd0e5b8f607deca462c60fdd7bac40c
4MN35T <113859255+4MN35T@users.noreply.github.com>
2023-04-06 03:05:39 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,40 +6,43 @@ function Register() {
     const [password, setPassword] = useState("");
     const [error, setError] = useState(false);
 
-    const formSubmint = (e) => {
-        e.preventDefault();
-        if (!email || !password) {
-            setError(true);
-            return;
-        }
-        setError(false);
-        const user = {
-            email: email,
-            password: password,
-        }
-        localStorage.setItem('user', JSON.stringify(user));
-        setEmail('');
-        setPassword('');
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
     }
 
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (email.length <= 0 || password.length <= 0) {
+            setError(true);
+        } else {
+            localStorage.setItem('user', JSON.stringify({ email, password }));
+            setError(false);
+            setEmail("");
+            setPassword("");
+        }
+    }
     return (
         <div className='register'>
             <h2>Register</h2>
-            <form onSubmit={formSubmint} className='sign-up-form'>
-                {error && <p className='error-para'>"Email or password isn't entered!"</p>}
+            <form className='sign-up-form' onSubmit={handleSubmit}>
+                {error ? <p className='error-para'>"Email or password isn't entered!"</p> : null}
                 <div className='email-div'>
                     <label htmlFor="email">Email: </label>
-                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" id="email" />
+                    <input type="email" id="email" value={email} onChange={handleEmailChange} autoComplete="off" />
                 </div>
                 <div className='password-div'>
                     <label htmlFor="password">Password: </label>
-                    <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" id="password" />
+                    <input type="password" id="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <button type="submit" className='register-btn'>Register</button>
             </form>
             <div>
                 Already have an account?
-                <Link to={'/login'}><button className='login-link'>Login</button></Link>
+                <Link to="/login"><button className='login-link'>Login</button></Link>
             </div>
         </div>
     )
