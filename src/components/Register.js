8f317c7970132147a5c4bf71f4c06bcf41b8b2e8
4MN35T <113859255+4MN35T@users.noreply.github.com>
2023-04-05 15:00:33 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,24 +6,40 @@ function Register() {
     const [password, setPassword] = useState("");
     const [error, setError] = useState(false);
 
+    const formSubmint = (e) => {
+        e.preventDefault();
+        if (!email || !password) {
+            setError(true);
+            return;
+        }
+        setError(false);
+        const user = {
+            email: email,
+            password: password,
+        }
+        localStorage.setItem('user', JSON.stringify(user));
+        setEmail('');
+        setPassword('');
+    }
+
     return (
         <div className='register'>
             <h2>Register</h2>
-            <form className='sign-up-form'>
-                <p className='error-para'>"Email or password isn't entered!"</p>
+            <form onSubmit={formSubmint} className='sign-up-form'>
+                {error && <p className='error-para'>"Email or password isn't entered!"</p>}
                 <div className='email-div'>
                     <label htmlFor="email">Email: </label>
-                    <input type="email" id="email" />
+                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" id="email" />
                 </div>
                 <div className='password-div'>
                     <label htmlFor="password">Password: </label>
-                    <input type="password" id="password" />
+                    <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" id="password" />
                 </div>
                 <button type="submit" className='register-btn'>Register</button>
             </form>
             <div>
                 Already have an account?
-                <Link><button className='login-link'>Login</button></Link>
+                <Link to={'/login'}><button className='login-link'>Login</button></Link>
             </div>
         </div>
     )
